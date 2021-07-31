3b93dc5bdd939083525c9a39305d1b51ec499d87
JastSuper <59697507+JastSuper@users.noreply.github.com>
2021-07-31 13:27:48 +0300

    fixed bags and write unit tests

--- TestFilter.py
+++ TestFilter.py
@@ -4,18 +4,39 @@ import filter
 
 class TestFilter(unittest.TestCase):
 
-    def test_first_text(self):
+    def test_first_text(self): # 1
         self.assertEqual(filter.text_x(
             ['sad', 'mad', 'bad'],
             "I'm love sad because i'm bad"),
             "I'm love *** because i'm ***"
         )
 
-    def test_second_text(self):
+    def test_second_text(self): # 2
         self.assertEqual(filter.text_x(
             ['sad', 'mad', 'bad'],
             "sad, mad, bad"),
             "***, ***, ***"
+        )
+
+    def test_second_text(self): # 3
+        self.assertEqual(filter.text_x(
+            ['sad,', 'mad,', 'bad,'],
+            "sad, mad, bad,"),
+            "**** **** ****"
+        )
+
+    def test_second_text(self): # 4
+        self.assertEqual(filter.text_x(
+            ['saded', 'mad', 'bad'],
+            "saded and, death"),
+            "***** and, death"
+        )
+
+    def test_second_text(self): # 5
+        self.assertEqual(filter.text_x(
+            ['sad', 'mad', 'bad!'],
+            "sad. and mad. want bad!."),
+            "***. and ***. want ****."
         )
 
 if __name__ == "__main__":
--- filter.py
+++ filter.py
@@ -1,14 +1,30 @@
 def text_x(bad_words, text):
-    result = text.replace(',', '').split()
+    result = text.split()
     for i in range(len(bad_words)):
         for j in range(len(result)):
             if result[j] == bad_words[i]:
-                result[j] = "*" * len(result[j])
-    if text.replace(',', '').split(' ') == text.split(', '):
-        # print(', '.join(result))
-        return ', '.join(result)
-    else:
-        # print(' '.join(result))
-        return ' '.join(result)
+                result[j] = result[j].replace(result[j], '*' * len(result[j]))
+            if result[j][-1] == ',' or result[j][-1] == '.':
+                if result[j][:-1] == bad_words[i]:
+                    result[j] = result[j].replace(result[j][:-1],
+                                                  '*' * (len(result[j]) - 1))
 
-text_x(['sad', 'mad', 'bad'], "I'm love sad because i'm bad")
+    return ' '.join(result)
+
+text_x(['sad', 'mad', 'bad'], "I'm love sad, because, i'm bad")
+
+
+# def text_x(bad_words, text):
+#     result = text.replace(',', '').split()
+#     for i in range(len(bad_words)):
+#         for j in range(len(result)):
+#             if result[j] == bad_words[i]:
+#                 result[j] = "*" * len(result[j])
+#     if text.replace(',', '').split(' ') == text.split(', '):
+#         # print(', '.join(result))
+#         return ', '.join(result)
+#     else:
+#         # print(' '.join(result))
+#         return ' '.join(result)
+#
+# text_x(['sad', 'mad', 'bad'], "I'm love sad because i'm bad")
