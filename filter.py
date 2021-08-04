ccc7e664b885fbb197c8092aa97841e3211e2e27
JastSuper <59697507+JastSuper@users.noreply.github.com>
2021-08-04 16:57:02 +0300

    modified function and write more tests

--- filter.py
+++ filter.py
@@ -2,15 +2,12 @@ BAD_WORDS = ['sad', 'mad', 'bad']
 
 def text_x(bad_words, text):
     result = text.split()
-    for i in range(len(bad_words)):
-        for j in range(len(result)):
-            if result[j].lower() == bad_words[i]:
-                result[j] = result[j].replace(result[j], '*' * len(result[j]))
-            if result[j][:-1].isalpha() == True:
-                if result[j][:-1].lower() == bad_words[i]:
-                    result[j] = result[j].replace(result[j][:-1],
-                                                  '*' * (len(result[j]) - 1))
-
+    for id in enumerate(bad_words):
+        for i, index in enumerate(result):
+            if index.lower() == id[1]:
+                result[i] = index.replace(index, '*' * len(index))
+            elif id[1] in index.lower():
+                result[i] = index.replace(index[:-(len(index) - len(id[1]))], '*' * len(id[1]))
     return ' '.join(result)
 
-text_x(BAD_WORDS, "Hello Bad")
+text_x(BAD_WORDS, "Hello Baded!!!!!")
--- filter_test.py
+++ filter_test.py
@@ -18,61 +18,83 @@ class TestFilter(unittest.TestCase):
             "***, ***, ***"
         )
 
-    def test_second_text(self): # 3
+    def test_third_text(self): # 3
         self.assertEqual(filter.text_x(
             filter.BAD_WORDS,
             "sad, mad, bad,"),
-            "**** **** ****"
+            "***, ***, ***,"
         )
 
-    def test_second_text(self): # 4
+    def test_fourth_text(self): # 4
         self.assertEqual(filter.text_x(
             filter.BAD_WORDS,
-            "saded and, death"),
-            "***** and, death"
+            "sad and, death"),
+            "*** and, death"
         )
 
-    def test_second_text(self): # 5
+    def test_fifth_text(self): # 5 -
         self.assertEqual(filter.text_x(
             filter.BAD_WORDS,
             "sad. and mad. want bad!."),
-            "***. and ***. want ****."
+            "***. and ***. want ***!."
         )
 
-    def test_second_text(self): # 6
+    def test_sixth_text(self): # 6
         self.assertEqual(filter.text_x(
             filter.BAD_WORDS,
             "Hello Bad"),
             "Hello ***"
         )
 
-    def test_second_text(self): # 7
+    def test_seventh_text(self): # 7
         self.assertEqual(filter.text_x(
             filter.BAD_WORDS,
             "Hello, Bad"),
             "Hello, ***"
         )
 
-    def test_second_text(self): # 8
+    def test_eighth_text(self): # 8
         self.assertEqual(filter.text_x(
             filter.BAD_WORDS,
             "Hello Bad!"),
             "Hello ***!"
         )
 
-    def test_second_text(self): # 9
+    def test_ninth_text(self): # 9
         self.assertEqual(filter.text_x(
             filter.BAD_WORDS,
             "Hello Bad?"),
             "Hello ***?"
         )
 
-    def test_second_text(self): # 10 
+    def test_tenth_text(self): # 10
         self.assertEqual(filter.text_x(
             filter.BAD_WORDS,
             "HELLO BAD"),
             "HELLO ***"
         )
+
+    def test_eleventh_text(self): # 11
+        self.assertEqual(filter.text_x(
+            filter.BAD_WORDS,
+            "HELLO BAD!!!!!!!!"),
+            "HELLO ***!!!!!!!!"
+        )
+
+    def test_twelfth_text(self): # 12
+        self.assertEqual(filter.text_x(
+            filter.BAD_WORDS,
+            "baded?"),
+            "***ed?"
+        )
+
+    def test_thirteenth_text(self): # 13
+        self.assertEqual(filter.text_x(
+            ['saded', 'maded', 'baded'],
+            "baded?"),
+            "*****?"
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
