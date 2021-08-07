9870f2cab95e06c35e9fc583316eb4785356bd34
JastSuper <59697507+JastSuper@users.noreply.github.com>
2021-08-07 12:18:13 +0300

    update func

--- filter.py
+++ filter.py
@@ -2,12 +2,12 @@ BAD_WORDS = ['sad', 'mad', 'bad']
 
 def text_x(bad_words, text):
     result = text.split()
-    for id in enumerate(bad_words):
-        for i, index in enumerate(result):
-            if index.lower() == id[1]:
-                result[i] = index.replace(index, '*' * len(index))
-            elif id[1] in index.lower():
-                result[i] = index.replace(index[:-(len(index) - len(id[1]))], '*' * len(id[1]))
+    for word in enumerate(bad_words):
+        for index, list_word in enumerate(result):
+            if list_word.lower() == word[1]:
+                result[index] = list_word.replace(list_word, '*' * len(list_word))
+            elif word[1] in list_word.lower():
+                result[index] = list_word.replace(list_word[:-(len(list_word) - len(word[1]))], '*' * len(word[1]))
     return ' '.join(result)
 
 text_x(BAD_WORDS, "Hello Baded!!!!!")
